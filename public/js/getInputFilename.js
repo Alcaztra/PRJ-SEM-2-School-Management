69fbb61b6368062512de3c57302865f5da51b960
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-20 16:21:55 +0700

    Create getInputFilename.js
    Fixing for input[file] not show file name.

---
+++ public/js/getInputFilename.js
@@ -0,0 +1,16 @@
+$('.custom-file-input').on('change', function (e) {
+    let fileName = $(this).val().split('\\').pop();
+    // $(this).next('.custom-file-label').html(fileName)
+    var nextSibling = e.target.nextElementSibling;
+    nextSibling.innerText = fileName;
+
+    let reader = new FileReader();
+    reader.onload = (e) => {
+        $('#preview_image').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(this.files[0]);
+})
+$('button[type=reset]').on('click', function () {
+    $('.custom-file-label').html('Profile image');
+    $('#preview_image').attr('src', '');
+})
