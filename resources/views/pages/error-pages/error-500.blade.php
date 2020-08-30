e35fcbe67e3513af03e23157695cf04fc5b36670
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-30 23:17:58 +0700

    Create error-500.blade.php

---
+++ resources/views/pages/error-pages/error-500.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.bg-particles')
+@section('page_title', '500 error')
+@section('sub-content')
+    <div class="container d-flex align-items-center justify-content-center">
+        <div class="row">
+            <div class="col text-uppercase" style="cursor: default">
+                <h1><span>500 Error</span> | <span class="text-warning">Internal Server Error</span></h1>
+            </div>
+        </div>
+    </div>
+@endsection
