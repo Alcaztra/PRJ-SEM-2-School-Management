7d9db94e2a56e6aee2d66d58911d6fe0af8a6613
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-30 22:55:55 +0700

    Update AdminFactory.php

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -7,12 +9,11 @@ $factory->define(App\Admin::class, function (Faker $faker) {
     return [
         'user_id' => 'Admin' . $faker->unique()->randomNumber(3),
         'name' => $faker->name,
-        'gender' => $faker->randomKey(['male', 'female', 'other']),
+        'gender' => $faker->randomElement(['male','female','other']),
         'email' => $faker->email,
         'phone' => $faker->phoneNumber,
-        'birthday' => $faker->dateTimeThisDecade,
+        'birthday' => $faker->date(),
         'address' => $faker->address,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
 });
