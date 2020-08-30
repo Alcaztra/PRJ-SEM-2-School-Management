e1f3820624e813c2fa27380ef958b9b636c5a8bb
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-30 11:20:49 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -76,5 +76,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
+        //
+        \Illuminate\Session\Middleware\StartSession::class,
     ];
 }
