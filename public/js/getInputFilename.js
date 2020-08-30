b0b3f40f3770478e5db1c1ecc263bd7648b72a3f
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-30 11:17:01 +0700

    Update getInputFilename.js

--- public/js/getInputFilename.js
+++ public/js/getInputFilename.js
@@ -6,7 +6,7 @@ $('.custom-file-input').on('change', function (e) {
 
     let reader = new FileReader();
     reader.onload = (e) => {
-        $('#preview_image').attr('src', e.target.result);
+        $('img#preview_image').attr('src', e.target.result);
     }
     reader.readAsDataURL(this.files[0]);
 })
