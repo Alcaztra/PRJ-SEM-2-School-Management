b1f593a201ff99fbd9fd0ce67363adfb6f9dc3d7
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-30 22:53:07 +0700

    create migrations

--- database/migrations/2020_08_30_133521_create_teacher_table.php
+++ database/migrations/2020_08_30_133521_create_teacher_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Hash;
 
-class CreateAdminsTable extends Migration
+class CreateTeacherTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->charset = 'utf8';
             $table->collation = 'utf8_general_ci';
             $table->string('user_id')->primary();
@@ -38,6 +38,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('teachers');
     }
 }
