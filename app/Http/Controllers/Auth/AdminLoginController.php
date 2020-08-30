85e002ad09e5170c17c598927afcaad224d018f4
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-30 22:52:40 +0700

    Update AdminLoginController.php

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -131,7 +131,7 @@ class AdminLoginController extends Controller
         //debug
         // dd('authenticated',Auth::guard('admin')->user(),Auth::guard('admin')->check());
         // dd(Auth::guard('admin')->login($user));
-        Session::put('test', $user);
+        // Session::put('test', $user);
         echo ('<script>alert("login success")</script>');
         return redirect('/dashboard');
     }
