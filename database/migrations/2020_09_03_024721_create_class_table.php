0e307d2d87401c9c958fc5e5bd1a0db375e94809
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-09-04 18:02:57 +0700

    Update 2020_09_03_024721_create_class_table.php

--- database/migrations/2020_09_03_024721_create_class_table.php
+++ database/migrations/2020_09_03_024721_create_class_table.php
@@ -17,8 +17,11 @@ class CreateClassTable extends Migration
             $table->charset = 'utf8';
             $table->collation = 'utf8_general_ci';
             $table->string('class_id')->primary();
-            $table->string('room');
-            $table->date('date_start');
+            $table->string('room')->nullable();
+            $table->string('course_id');
+            $table->integer('total_duarion');
+            $table->date('start_day');
+            $table->integer('step')->default(2);
             $table->timestamps();
         });
     }
